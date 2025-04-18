apache/tomcat

Adding nonstandard support for c:set and c:remove

java/org/apache/jasper/runtime/JspRuntimeLibrary.java
@@ -24,6 +24,7 @@ import java.lang.reflect.Method;
 import java.net.URLEncoder;
 import java.util.Enumeration;
 
+import javax.el.VariableMapper;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -967,5 +968,31 @@ public class JspRuntimeLibrary {
             log.warn(Localizer.getMessage("jsp.warning.tagPreDestroy", tag.getClass().getName()), t);
         }
 
+    }
+
+    /**
+     * This method parallels the logic of {@code SetSupport.doEndTag()}.
+     *
+     * @param pageContext pageContext
+     * @param var name of the variable
+     * @param value value to store
+     * @param scope scope
+     */
+    public static void nonstandardSetTag(javax.servlet.jsp.PageContext pageContext, String var, Object value,
+            int scope) {
+        if (value == null) {
+            // matches SetTag and removes the key from the specified scope
+            pageContext.removeAttribute(var, scope);
+        } else {
+            if (scope == PageContext.PAGE_SCOPE) {
+                // matches SetTag and cleans up the VariableMapper
+                VariableMapper vm = pageContext.getELContext().getVariableMapper();
+                if (vm != null) {
+                    vm.setVariable(var, null);
+                }
+            }
+            // does the all-important set of the correct scope
+            pageContext.setAttribute(var, value, scope);
+        }
     }
 }

test/org/apache/jasper/runtime/TestJspRuntimeLibrary.java
@@ -16,13 +16,20 @@
  */
 package org.apache.jasper.runtime;
 
+import javax.el.ELManager;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.TesterPageContextWithAttributes;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import org.apache.catalina.startup.TomcatBaseTest;
 import org.apache.tomcat.util.buf.ByteChunk;
 
 public class TestJspRuntimeLibrary extends TomcatBaseTest {
+
+    private PageContext pageContext;
 
     /*
      * Tests successful conversions
@@ -139,5 +146,35 @@ public class TestJspRuntimeLibrary extends TomcatBaseTest {
     // Assertion for text contained with <p></p>, e.g. printed by tags:echo
     private static void assertEcho(String result, String expected) {
         Assert.assertTrue(result, result.indexOf("<p>" + expected + "</p>") > 0);
+    }
+
+    @Before
+    public void setupTestVars() {
+        pageContext = new TesterPageContextWithAttributes((new ELManager()).getELContext());
+    }
+
+    @Test
+    public void testNonstandardSetWithUndefinedScope() {
+        JspRuntimeLibrary.nonstandardSetTag(pageContext, "var", "value", PageContext.PAGE_SCOPE);
+        Assert.assertEquals("value", pageContext.getAttribute("var"));
+        Assert.assertEquals("value", pageContext.getAttribute("var", PageContext.PAGE_SCOPE));
+        Assert.assertEquals(null, pageContext.getAttribute("var", PageContext.REQUEST_SCOPE));
+
+        JspRuntimeLibrary.nonstandardSetTag(pageContext, "var", null, PageContext.PAGE_SCOPE);
+        Assert.assertEquals(null, pageContext.getAttribute("var"));
+
+    }
+
+    @Test
+    public void testNonstandardSetWithDefinedScope() {
+        final int[] scopes = {PageContext.PAGE_SCOPE, PageContext.REQUEST_SCOPE, PageContext.SESSION_SCOPE, PageContext.APPLICATION_SCOPE};
+        for (int scope : scopes) {
+            JspRuntimeLibrary.nonstandardSetTag(pageContext, "var", "value", scope);
+            Assert.assertEquals("value", pageContext.getAttribute("var", scope));
+
+            JspRuntimeLibrary.nonstandardSetTag(pageContext, "var", null, scope);
+            Assert.assertEquals(null, pageContext.getAttribute("var", scope));
+        }
+
     }
 }

jengebr/tomcat9

java/org/apache/jasper/runtime/JspRuntimeLibrary.java
@@ -25,6 +25,8 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 
+
+import jakarta.el.VariableMapper;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
@@ -963,5 +965,31 @@ public class JspRuntimeLibrary {
             log.warn(Localizer.getMessage("jsp.warning.tagPreDestroy", tag.getClass().getName()), t);
         }
 
+    }
+
+    /**
+     * This method parallels the logic of {@code SetSupport.doEndTag()}.
+     *
+     * @param pageContext pageContext
+     * @param var name of the variable
+     * @param value value to store
+     * @param scope scope
+     */
+    public static void nonstandardSetTag(jakarta.servlet.jsp.PageContext pageContext, String var, Object value,
+            int scope) {
+        if (value == null) {
+            // matches SetTag and removes the key from the specified scope
+            pageContext.removeAttribute(var, scope);
+        } else {
+            if (scope == PageContext.PAGE_SCOPE) {
+                // matches SetTag and cleans up the VariableMapper
+                VariableMapper vm = pageContext.getELContext().getVariableMapper();
+                if (vm != null) {
+                    vm.setVariable(var, null);
+                }
+            }
+            // does the all-important set of the correct scope
+            pageContext.setAttribute(var, value, scope);
+        }
     }
 }

test/org/apache/jasper/runtime/TestJspRuntimeLibrary.java
@@ -16,13 +16,20 @@
  */
 package org.apache.jasper.runtime;
 
+import jakarta.el.ELManager;
+import jakarta.servlet.jsp.PageContext;
+import jakarta.servlet.jsp.TesterPageContextWithAttributes;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import org.apache.catalina.startup.TomcatBaseTest;
 import org.apache.tomcat.util.buf.ByteChunk;
 
 public class TestJspRuntimeLibrary extends TomcatBaseTest {
+
+    private PageContext pageContext;
 
     /*
      * Tests successful conversions
@@ -139,5 +146,35 @@ public class TestJspRuntimeLibrary extends TomcatBaseTest {
     // Assertion for text contained with <p></p>, e.g. printed by tags:echo
     private static void assertEcho(String result, String expected) {
         Assert.assertTrue(result, result.indexOf("<p>" + expected + "</p>") > 0);
+    }
+
+    @Before
+    public void setupTestVars() {
+        pageContext = new TesterPageContextWithAttributes((new ELManager()).getELContext());
+    }
+
+    @Test
+    public void testNonstandardSetWithUndefinedScope() {
+        JspRuntimeLibrary.nonstandardSetTag(pageContext, "var", "value", PageContext.PAGE_SCOPE);
+        Assert.assertEquals("value", pageContext.getAttribute("var"));
+        Assert.assertEquals("value", pageContext.getAttribute("var", PageContext.PAGE_SCOPE));
+        Assert.assertEquals(null, pageContext.getAttribute("var", PageContext.REQUEST_SCOPE));
+
+        JspRuntimeLibrary.nonstandardSetTag(pageContext, "var", null, PageContext.PAGE_SCOPE);
+        Assert.assertEquals(null, pageContext.getAttribute("var"));
+
+    }
+
+    @Test
+    public void testNonstandardSetWithDefinedScope() {
+        final int[] scopes = {PageContext.PAGE_SCOPE, PageContext.REQUEST_SCOPE, PageContext.SESSION_SCOPE, PageContext.APPLICATION_SCOPE};
+        for (int scope : scopes) {
+            JspRuntimeLibrary.nonstandardSetTag(pageContext, "var", "value", scope);
+            Assert.assertEquals("value", pageContext.getAttribute("var", scope));
+
+            JspRuntimeLibrary.nonstandardSetTag(pageContext, "var", null, scope);
+            Assert.assertEquals(null, pageContext.getAttribute("var", scope));
+        }
+
     }
 }